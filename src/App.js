437cb165faa7dd347d2e52cced080f2a785b9596
SonaliChaudhari <160842768+Sonali-Chaudhri@users.noreply.github.com>
2024-08-13 11:33:07 +0530

    react-docker-app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+          Welcome to my page..!!
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Happy Learning!!
         </a>
       </header>
     </div>
